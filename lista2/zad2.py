4030645a1bb3318389f960b34b93dc612b21324e
Your Name <you@example.com>
2020-03-16 16:34:30 +0100

    zad2

--- lista2/zad2.py
+++ lista2/zad2.py
@@ -1,9 +1,11 @@
+import sys
+
 def encode(file_name):
     table = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
     try:
         with open(file_name, 'r') as file:
-            bit_string = ""
-            result = ""
+            bit_string = ""  # wczytany tekst z pliku jako ciąg bitów
+            result = ""  # zakodowany ciąg
             for byte in file.read():
                 bits = bin(ord(byte))[2:]  # [2:] odcina dwa pierwsze znaki z zapisu binarnego 0b1010000
                 bits = "00000000"[len(bits):] + bits  # dopisuje z przodu zera, żeby było osiem znaków
@@ -26,6 +28,33 @@ def decode(file_name):
     table = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
     try:
         with open(file_name) as file:
-            
+            bit_string = ""  # odkodowany ciąg w postaci ciągu bitów
+            result = ""  # odkodowany ciąg znaków
+            for byte in file.read():
+                if byte != '=':
+                    bits = bin(table.index(byte))[2:]
+                    bits = "000000"[len(bits):] + bits
+                    bit_string += bits
+            while len(bit_string) >= 8:
+                bits = bit_string[:8]
+                bit_string = bit_string[8:]
+                result += chr(int(bits, base=2))
+            return result
     except OSError:
         print("Błędne dane")
+
+
+if __name__ == "__main__":
+    try:
+        if sys.argv[1] == "--encode":
+            with open(sys.argv[3], 'w') as output_file:
+                output_file.write(encode(sys.argv[2]))
+        elif sys.argv[1] == "--decode":
+            with open(sys.argv[3], 'w') as output_file:
+                output_file.write(decode(sys.argv[2]))
+        else:
+            print("Sposób wywołania programu:\n python zad2.py --encode plik_wejściowy plik_wyjściowy\n",
+                  "lub\n", "python zad2.py --decode plik_wejściowy plik_wyjściowy", sep='')
+    except IndexError:
+        print("Sposób wywołania programu:\n python zad2.py --encode plik_wejściowy plik_wyjściowy\n",
+                  "lub\n", "python zad2.py --decode plik_wejściowy plik_wyjściowy", sep='')
